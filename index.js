d5835651296cd01124668fa2285b6065093049b6
JoseK03 <dajoseva@gmail.com>
2023-09-14 07:13:47 -0500

    feat: :rocket: Estructuracion de carpetas, inicio de proyecto

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const routerBase = require('./routes/routes.js');
+
+require('dotenv').config();
+const port = process.env.PORT;
+app.use(express.json());
+app.listen(port,()=>{
+    console.log('Servidor inicializado');
+});
+
+app.use('/eps',routerBase);
